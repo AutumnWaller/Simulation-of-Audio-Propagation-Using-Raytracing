ce4b15e59440948fed9bbd22109ad6c1a1be8ea9
Dan <danwaller97@gmail.com>
2021-02-12 15:20:25 +0000

    Added listener to the player
    Changed the audio struct to include more information about bounces and such.
    Created a method in listener to play music passed to by the ray.

---
+++ Assets/Listener.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Listener : MonoBehaviour
+{
+    AudioSource source;
+    private void Awake()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void AudioRayHit(RT.AudioRay _ray)
+    {
+        if (source.isPlaying)
+            return;
+        source.volume = 20 * Mathf.Log10(_ray.distanceFromSource / 1.0f);
+        //print(_ray.currVolume);
+        source.clip = _ray.clip;
+        source.Play();
+    }
+}
--- Assets/RT.cs
+++ Assets/RT.cs
@@ -1,32 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RT : MonoBehaviour
 {
+    public struct AudioRay
+    {
+        public Ray prevRay;
+        public Ray currRay;
+        public AudioClip clip;
+        public float distanceFromSource;
+        public float currVolume;
+        public float i1, i2;
+        public float r1, r2;
+    }
+
+    //Cache the distance from the source, the amount of bounces, the properties of those bounces into a texture.
     public ComputeShader rtShader;
     public Texture2D backgroundTexture;
+    public Text display;
     private RenderTexture renderTarget;
 
-    List<Ray> rays;
+    public AudioClip clip;
+    List<AudioRay> rays;
     List<RaycastHit> hits;
-    public int maxBounces = 10;
-    void Start()
-    {
+    public int max = 500;
+    private int maxBounces = 500;
+    public Vector3 direction = Vector3.zero;
 
+    private void Start()
+    {
+        
     }
 
     void FixedUpdate()
     {
-        rays = new List<Ray>();
+        direction = transform.forward;
+        display.text = transform.forward.ToString();
+        maxBounces = max;
+        rays = new List<AudioRay>();
         hits = new List<RaycastHit>();
         RaycastHit hit;
-        Ray r = new Ray(transform.position, transform.forward);
+        Ray r = new Ray(transform.position, direction);
+        AudioRay ar = new AudioRay();
+        ar.currRay = r;
+        ar.clip = clip;
+        ar.currVolume = 1;
         if (Physics.Raycast(r, out hit, 100, ~8))
         {
-            rays.Add(r);
+            ar.distanceFromSource = hit.distance;
+            ar.i1 = ar.distanceFromSource;
+
+            rays.Add(ar);
             hits.Add(hit);
-            RayTrace(r, hit, maxBounces);
+            RayTrace(ar, hit, maxBounces);
+            //display.text = hits.Count.ToString();
         }
     }
 
@@ -34,26 +63,44 @@ public class RT : MonoBehaviour
     {
         if (rays != null)
         {
-            for(int i = 0; i < rays.Count - 1; i++)
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawCube(transform.position, Vector3.one);
+            for (int i = 0; i < rays.Count - 1; i++)
             {
-                Debug.DrawRay(rays[i].origin,  rays[i].direction.normalized * hits[i].distance, Color.Lerp(Color.green, Color.red, (float)i / rays.Count));
+                Debug.DrawRay(rays[i].currRay.origin,  rays[i].currRay.direction.normalized * hits[i].distance, Color.Lerp(Color.green, Color.red, (float)i / rays.Count));
             }
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawCube(rays[rays.Count - 1].currRay.origin, Vector3.one);
 
         }
     }
 
-    void RayTrace(Ray _prevRay, RaycastHit _prevHit, int _bouncesLeft)
+
+
+    void RayTrace(AudioRay _ray, RaycastHit _prevHit, int _bouncesLeft)
     {
         if (_bouncesLeft <= 0)
             return;
-        Ray r = new Ray(_prevHit.point,  Vector3.Reflect(_prevRay.direction.normalized, _prevHit.normal));
-        rays.Add(r);
+        Ray r = new Ray(_prevHit.point,  Vector3.Reflect(_ray.currRay.direction.normalized, _prevHit.normal));
         RaycastHit hit;
 
         if (Physics.Raycast(r, out hit, 100, ~8))
         {
+            if (hit.transform.tag == "Player")
+            {
+                Listener l = hit.transform.GetComponent<Listener>();
+                l.AudioRayHit(_ray);
+                return;
+            }
+            AudioRay newRay = new AudioRay();
+            newRay.prevRay = _ray.currRay;
+            newRay.currRay = r;
+            newRay.clip = _ray.clip;
+            newRay.distanceFromSource = _ray.distanceFromSource + (hit.distance);
+            newRay.currVolume = _ray.currVolume - 20 * Mathf.Log10(newRay.distanceFromSource / _ray.distanceFromSource);
+            rays.Add(newRay);
             hits.Add(hit);
-            RayTrace(r, hit, _bouncesLeft - 1);
+            RayTrace(newRay, hit, _bouncesLeft - 1);
         }
     }
 
