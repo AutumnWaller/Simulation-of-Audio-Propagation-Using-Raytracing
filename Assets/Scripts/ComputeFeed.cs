188f845043e9c7a4e12752e6bdbafa9299ef4792
Dan <danwaller97@gmail.com>
2021-03-04 18:33:22 +0000

    RT shader WIP
    Working on cuboid collision

---
+++ Assets/Scripts/ComputeFeed.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputeFeed : MonoBehaviour
+{
+    public ComputeShader m_rtShader;
+    public Texture2D m_skyboxTexture;
+    private RenderTexture m_renderTarget;
+
+    private Camera m_camera;
+    private void Awake()
+    {
+        m_camera = GetComponent<Camera>();
+    }
+
+    private void SetShaderData()
+    {
+        m_rtShader.SetMatrix("worldMat", m_camera.cameraToWorldMatrix);
+        m_rtShader.SetMatrix("inverseProjMat", m_camera.projectionMatrix.inverse);
+        m_rtShader.SetTexture(0, "skyboxTexture", m_skyboxTexture);
+    }
+
+    private void OnRenderImage(RenderTexture _source, RenderTexture _destination)
+    {
+        SetShaderData();
+        Render(_destination);
+    }
+
+    private void SetRenderTarget()
+    {
+        if(m_renderTarget == null || m_renderTarget.width != Screen.width || m_renderTarget.height != Screen.height)
+        {
+            if (m_renderTarget != null)
+                m_renderTarget.Release();
+            m_renderTarget = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            m_renderTarget.enableRandomWrite = true;
+            m_renderTarget.Create();
+        }
+    }
+
+    private void Render(RenderTexture _renderDestination)
+    {
+        SetRenderTarget();
+        m_rtShader.SetTexture(0, "Result", m_renderTarget);
+        m_rtShader.Dispatch(0, Mathf.CeilToInt(Screen.width / 8), Mathf.CeilToInt(Screen.height / 8), 1);
+        Graphics.Blit(m_renderTarget, _renderDestination);
+    }
+
+}
