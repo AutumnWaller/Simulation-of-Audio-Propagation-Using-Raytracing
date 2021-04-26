1703dc8730112d7ee903a6cd4564d9b960b95bdc
Dan <danwaller97@gmail.com>
2021-04-26 19:18:03 +0100

    Fixed RT shader, got rid of old code
    Made audio play after receiving ray info from the shader and running a single ray on the CPU

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public List<AudioClip> m_audioClips = new List<AudioClip>();
+    private static AudioSource m_audioSource;
+
+    private void Awake() {
+        m_audioSource = GetComponent<AudioSource>();
+        m_audioSource.clip = m_audioClips[0];
+    }
+
+    public static void Play(float _distance){
+        if(m_audioSource.isPlaying)
+            return;
+        m_audioSource.volume = (1 / (_distance * _distance) * 100);
+
+        m_audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/PlayRayAudio.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRayAudio : MonoBehaviour
+{
+    public void PlayAudio(float _distance){
+        AudioManager.Play(_distance);
+    }
+}
